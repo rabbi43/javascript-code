893ae18e9dc3ef40f391542cf76cd3361112b7f4
Fazle Rabbi <96676124+rabbi43@users.noreply.github.com>
2022-03-23 10:12:08 +0600

    Create 1darray.js

---
+++ array/1darray.js
@@ -0,0 +1,18 @@
+// create a function
+function highestScore(){
+	//document.write(names);
+	var max = names[0];
+	for(x=1;x<names.length;x++){// loop start from one/1 because already max=0;
+		if(max < names[x]){//condition
+			max = names[x];// the max value/digit
+		}
+	}
+	return max;//return the max value
+	
+}
+
+
+var names = [33,700,89,99];//1dimentional array
+var received = highestScore(names);//max value received from return...
+document.write(received);//print the max value
+
