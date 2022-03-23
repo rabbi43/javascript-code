7d39876fcc1e50b6a3ecdaf71db0019ef7627e4b
Fazle Rabbi <96676124+rabbi43@users.noreply.github.com>
2022-03-23 15:47:43 +0600

    Create math_object.js

---
+++ DataTypes/math_object.js
@@ -0,0 +1,16 @@
+// 23 March,2022
+// Author: Fazle Rabbi
+// this function created just for use break
+function brk(){
+	document.write("</br>");
+}
+
+
+// Math object
+// sqrt()squire,abs()absolute,pow()power,sin(),cos(),
+// floor()before int value,ceil()after int value,random(),
+// max()
+var x = 100;
+var y = 20;
+var result = Math.max(x,y);
+document.write(result);
