13b494d9e04efb34c4415ceeded68de872196934
Fazle Rabbi <96676124+rabbi43@users.noreply.github.com>
2022-03-23 10:53:02 +0600

    Create iffes.js

---
+++ function/iffes.js
@@ -0,0 +1,14 @@
+//IIFES 
+// immediately call a function
+(function names(msg){
+	document.write(msg);
+})("Hello Geek!");
+
+brk();
+
+// function expression
+var x = function names(msg){
+	document.write(msg);
+};
+
+x("hello!");
