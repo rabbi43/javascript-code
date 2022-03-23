8239e3a178b801292c984c178c3b63585624c731
Fazle Rabbi <96676124+rabbi43@users.noreply.github.com>
2022-03-23 10:40:01 +0600

    Create function.js

---
+++ function/function.js
@@ -0,0 +1,17 @@
+// 23 March,2022
+// Author: Fazle Rabbi
+// creating function
+// notes: create function,call function,value passing,return value
+function addition(x,y){//x&y are the parametter for recevied a value 
+	var sum = x+y; // =8
+	//document.write(sum);
+	return sum;
+}
+
+// calling function
+//addition(3,5);//x=3 & y=5 ; x+y=3+5=8
+// 3 & 5 are value of x & y...
+
+var rt = addition(5,5);//create for teceive return value
+document.write(rt);
+
